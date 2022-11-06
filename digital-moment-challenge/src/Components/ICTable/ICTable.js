897f9b7cc9200a1d93cea9cf6e9b7bb54098af27
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2022-11-06 13:50:13 -0500

    Clean project from material table

--- digital-moment-challenge/src/Components/ICTable/ICTable.js
+++ digital-moment-challenge/src/Components/ICTable/ICTable.js
@@ -1,31 +1,7 @@
 import React, {Component} from "react";
-import MaterialTable from "material-table";
 import { ICTableRow } from "./ICTableElements";
 
 export const ICTable = ({realData}) =>{
-    const data = [
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-        { title: "Mock Challenge", author: "Mock Author", description: "Mock Description", localisation: "Mock Localisation", upvotes: 33},
-    ];
-    const columns = [
-        {
-            render: (row) => { <ICTableRow rowData={row}/>  }
-        }
-    ];
-    return(
-        <MaterialTable title="Test Table" columns={columns} data={data}/>
-    )
+    
+    console.log(realData);
 }
