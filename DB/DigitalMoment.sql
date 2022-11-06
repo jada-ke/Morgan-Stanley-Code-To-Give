8d0a4330b30a4fb5520ebdc75095bdb20e7c4350
Anirudh Boddu <36669000+AnirudhBoddu@users.noreply.github.com>
2022-11-05 23:28:31 -0400

    Updated DigitalMoment.sql
    Added Latitude and Longitude columns to the IDEAS and CHALLENGES tables. Also, added queries to insert test data into the tables.

--- DB/DigitalMoment.sql
+++ DB/DigitalMoment.sql
@@ -22,27 +22,27 @@ CREATE TABLE ideas (ID INT Primary key NOT NULL, UserID TEXT NOT NULL,
 					DatePosted DATE NOT NULL, UpVotes INT NOT NULL, 
 					DownVotes INT NOT NULL, Description TEXT NOT NULL,
 					Postal_code VARCHAR(30) NOT NULL, City TEXT, 
-					Country TEXT, Tag TEXT, UNRefID INT, 
+					Country TEXT, Tag TEXT, Lat FLOAT NOT NULL, Long FLOAT NOT NULL, UNRefID INT, 
 					CONSTRAINT fk_UN_SNG FOREIGN KEY (UNRefID) REFERENCES UN_SNG(ID));
 
 CREATE TABLE challenges (ID INT Primary key NOT NULL, UserID TEXT NOT NULL,
 						 DatePosted DATE NOT NULL, UpVotes INT NOT NULL, 
 						 DownVotes INT NOT NULL, Description TEXT NOT NULL,
 						 Postal_code VARCHAR(30) NOT NULL, City TEXT, 
-						 Country TEXT, Tag TEXT, UNRefID INT, 
+						 Country TEXT, Lat FLOAT NOT NULL, Long FLOAT NOT NULL, Tag TEXT, UNRefID INT, 
 						 CONSTRAINT fk_UN_SNG FOREIGN KEY (UNRefID) REFERENCES UN_SNG(ID));
 
-insert into ideas values(100,'Anirudh','2022-11-05',7,13,'SAMPLE','H2X 2V1','Montreal','Canada','Environment');
-insert into ideas values(101,'John Doe','2022-11-03',6,2,'Test','H3Y 2V4','Calgary','Canada','Poverty');
-insert into ideas values(102,'Jane Doe','2022-11-01',3,9,'SampleTest','H4X 2V2','Waterloo','Canada','Inequality');
-insert into ideas values(103,'Aegon','2022-11-04',9,11,'TestSample','H3X 3V2','New York','USA','Electricity');
-insert into ideas values(104,'Aemond','2022-11-05',0,1,'Something','H3Z 2V2','Dallas','USA','Peace');
+insert into ideas values(100,'Anirudh','2022-11-05',7,13,'SAMPLE','H2X 2V1','Montreal','Canada',45.5019,73.5674,'Environment');
+insert into ideas values(101,'John Doe','2022-11-03',6,2,'Test','H3Y 2V4','Calgary','Canada',51.0447,114.0719,'Poverty');
+insert into ideas values(102,'Jane Doe','2022-11-01',3,9,'SampleTest','H4X 2V2','Waterloo','Canada',43.4643,80.5204,'Inequality');
+insert into ideas values(103,'Aegon','2022-11-04',9,11,'TestSample','H3X 3V2','New York','USA',40.7128,74.0060,'Electricity');
+insert into ideas values(104,'Aemond','2022-11-05',0,1,'Something','H3Z 2V2','Dallas','USA',32.7767,96.7970,'Peace');
 
-insert into challenges values(200,'Jon Snow','2022-11-05',8,13,'SAMPLE','H2X 2V1','Quebec City','Canada','Peace');
-insert into challenges values(201,'Stark','2022-12-03',6,12,'Test','H3Y 2V4','Vancouver','Canada','Water');
-insert into challenges values(202,'Targeryan','2022-12-01',13,9,'SampleTest','H4X 2V2','Burnaby','Canada','Inequality');
-insert into challenges values(203,'HighTower','2022-12-04',9,21,'TestSample','H3X 3V2','San Fransisco','USA','Environment');
-insert into challenges values(204,'Baratheon','2022-12-05',10,13,'Something','H3Z 2V2','Houston','USA','Peace');
+insert into challenges values(200,'Jon Snow','2022-11-05',8,13,'SAMPLE','H2X 2V1','Quebec City','Canada',46.8131,71.2075,'Peace');
+insert into challenges values(201,'Stark','2022-12-03',6,12,'Test','H3Y 2V4','Vancouver','Canada',49.2827,123.1207,'Water');
+insert into challenges values(202,'Targeryan','2022-12-01',13,9,'SampleTest','H4X 2V2','Burnaby','Canada',49.2488,122.9805,'Inequality');
+insert into challenges values(203,'HighTower','2022-12-04',9,21,'TestSample','H3X 3V2','San Fransisco','USA',37.7749,122.4194,'Environment');
+insert into challenges values(204,'Baratheon','2022-12-05',10,13,'Something','H3Z 2V2','Houston','USA',29.7604,95.3698,'Peace');
 
 COMMIT;
 
