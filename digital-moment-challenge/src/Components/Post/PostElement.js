2ee3565331033a78cd1ae570bd93e0c431ea0cb7
Jada Ke Thompson <jada.thompson915@gmail.com>
2022-11-05 21:08:27 -0400

    tried making post element

--- digital-moment-challenge/src/Components/Post/PostElement.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Post = styled.post `
-    background-color: #ebfbff;
-    width: 1000 px;
-    max-width: 100%;
-    padding: 40px 40 px;
-`
-
--- digital-moment-challenge/src/Components/Post/index.js
+++ digital-moment-challenge/src/Components/Post/index.js
@@ -1,2 +1,33 @@
 import React from "react";
+import {Button} from "./Button/index"
+import styles from "./PostElement.css";
+
+const PostElement = () => {
+    return (
+        <div class= "wrapper">
+            <div class = "box a">
+                 <Button >Upvote </Button>
+            </div>
+            <div class = "box b"> 
+                <Button> Downvote</Button>
+            </div>
+            <div class = "box c"> thread name</div>
+            <div class = "box d"> Username/publish time</div>
+            <div class = "box e"> 
+                <Button>Suscribe</Button>
+            </div>
+            <div class = "box f"> Post text</div>
+            <div class = "box g"> 
+                <Button>Like</Button>
+            </div>
+            <div class = "box h"> 
+                <Button>Comment</Button>
+            </div>
+            <div class = "box i"> Extra</div>
+        </div>
+    )
+}
+
+export default PostElement
+
 
