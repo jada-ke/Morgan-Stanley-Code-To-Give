9187b32cafba5a45b3ca32094d1b7660f5f473c2
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2022-11-05 22:53:10 -0400

    Center the map

--- digital-moment-challenge/src/Components/Map/Map.js
+++ digital-moment-challenge/src/Components/Map/Map.js
@@ -18,7 +18,7 @@ export class Map extends Component {
                     crossorigin="" 
                 />
             </head>
-            <ICMapContainer center={[45.4, -75.7]} zoom={13} scrollWheelZoom={true}>
+            <ICMapContainer center={[0,0]} zoom={2} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
