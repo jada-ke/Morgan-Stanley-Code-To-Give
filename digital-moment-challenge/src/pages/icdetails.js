7d58184043e70ebd7de256af63b50fe01ad65a40
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2022-11-06 20:36:06 -0500

    Create icdetails.js

---
+++ digital-moment-challenge/src/pages/icdetails.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {ICTitle,ICDescription,styles} from "../Components/ICDetails";
+import {CommentSection} from 'react-comments-section'
+import 'react-comments-section/dist/index.css'
+
+const ICDetails = ({item}) => {
+    return (
+        <>
+            <div stye={styles}>
+            <ICTitle>
+                {item.title}
+            </ICTitle>
+            <ICDescription>
+                {item.description}
+            </ICDescription>
+            <div>By: {item.UserID}</div>
+            <div>Launched: {item.DatePosted}</div>
+            </div>
+
+            <CommentSection
+                currentUser={null}
+                commentData={this.state.data}
+                onSubmitAction={(data) => this.onSubmitAction(data)}
+                customNoComment={() => <div className='no-com'>No comments yet...</div>}
+                logIn={{
+                loginLink: 'http://localhost:3001/signin',
+                signupLink: 'http://localhost:3001/signup'
+                }}
+            />
+        </>
+    )
+}
+
+export default ICDetails
