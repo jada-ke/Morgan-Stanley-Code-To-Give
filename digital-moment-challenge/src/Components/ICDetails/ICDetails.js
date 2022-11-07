2278cd1c4bd74d3e396adaa4d37f787e81ae26bd
JeddSJr <55459056+JeddSJr@users.noreply.github.com>
2022-11-06 19:59:36 -0500

    Update ICDetails.js

--- digital-moment-challenge/src/Components/ICDetails/ICDetails.js
+++ digital-moment-challenge/src/Components/ICDetails/ICDetails.js
@@ -1,11 +1,12 @@
 import React from "react";
-import {ICTitle,ICDescription} from "./ICDetailsElements";
+import {ICTitle,ICDescription,styles} from "./ICDetailsElements";
 import {CommentSection} from 'react-comments-section'
 import 'react-comments-section/dist/index.css'
 
 const ICDetails = ({item}) => {
     return (
         <>
+            <div stye={styles}>
             <ICTitle>
                 {item.title}
             </ICTitle>
@@ -13,6 +14,8 @@ const ICDetails = ({item}) => {
                 {item.description}
             </ICDescription>
             <div>By: {item.UserID}</div>
+            <div>Launched: {item.DatePosted}</div>
+            </div>
 
             <CommentSection
                 currentUser={null}
